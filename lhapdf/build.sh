e9dd5d85a11418ac93621a3e1b5000f751343397
Zahari <zaharid@gmail.com>
2016-12-18 12:26:37 +0100

    xxx

--- lhapdf/build.sh
+++ lhapdf/build.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-# Depending on our platform, shared libraries end with either .so or .dylib
-if [[ `uname` == 'Darwin' ]]; then
-    # Also, included a workaround so that `-stdlib=c++` doesn't go to
-    # `gfortran` and cause problems.
-    #
-    # https://github.com/conda-forge/toolchain-feedstock/pull/8
-    DYLIB_EXT=dylib
-    export CFLAGS="${CFLAGS} -stdlib=libc++ -lc++"
-    export LDFLAGS="-headerpad_max_install_names -undefined dynamic_lookup -bundle -Wl,-search_paths_first -lc++"
-else
-    DYLIB_EXT=so
-    unset LDFLAGS
-fi
 ./configure --prefix=$PREFIX 
 make -j${CPU_COUNT}
 make install
