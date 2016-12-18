873a5bf7e299b82d92307b9577563b8243dff580
Zahari <zaharid@gmail.com>
2016-12-18 02:00:19 +0100

    Add experimental travis build

--- yaml-cpp/build.sh
+++ yaml-cpp/build.sh
@@ -4,6 +4,7 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DBoost_INCLUDE_DIR=$PREFIX/include \
     -DBUILD_SHARED_LIBS=ON \
+	-D_GLIBCXX_USE_CXX11_ABI=1\
     ..
-make
+make VERBOSE=1
 make install
