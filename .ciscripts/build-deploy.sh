2161abba4d637d41c2b9ec50f3b4b5f273394926
Zahari <zaharid@gmail.com>
2016-12-18 13:15:17 +0100

    Try to upload

---
+++ .ciscripts/build-deploy.sh
@@ -0,0 +1,26 @@
+#/usr/bin/bash
+
+#Find conda
+source ~/.bashrc
+conda build -q $1
+if [ $? != 0 ]; then
+	echo failed to build
+	exit 1
+fi
+
+
+echo "Uploading package to zigzah"
+KEY=$( mktemp )
+#This is defined in the Gitlab variables, under the Settings Menu.
+echo "$ZIGZAH_SSH_KEY" | base64 --decode > "$KEY"
+
+scp -i "$KEY" -o StrictHostKeyChecking=no\
+    ${CONDA_PATH}/conda-bld/${OUTPUT_ARCH}/*.tar.bz2 \
+    dummy@zigzah.com:~/conda-pkgs-private/${OUTPUT_ARCH}
+
+if [ $? == 0 ]; then
+	echo "Upload suceeded"
+else
+	echo "Upload failed"
+	exit 1
+fi
