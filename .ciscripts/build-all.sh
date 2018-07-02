a0ff33be5005b81914a1084129ab8910e6c24906
Zahari <zaharid@gmail.com>
2018-07-02 15:35:27 +0100

    Simplify build
    This has the advantage that dependencies such as LHAPDF --> APFEL
    should be resolved.

--- .ciscripts/build-all.sh
+++ .ciscripts/build-all.sh
@@ -2,9 +2,21 @@
 set -e
 set -u
 
-for recipe in \
-$(find . -maxdepth 1 -not -path './\.*' -not -path . -type d);\
-do
-echo "Building ${recipe}"
-.ciscripts/build-deploy.sh "$recipe"
-done
+conda build .
+
+echo "Uploading package to zigzah"
+#Idiotic mac mktemp
+KEY=$( mktemp  "${TMPDIR:-/tmp}/key.XXXXXXXXX" )
+#This is defined in the Gitlab variables, under the Settings Menu.
+echo "$ZIGZAH_SSH_KEY" | base64 --decode > "$KEY"
+
+scp -i "$KEY" -o StrictHostKeyChecking=no\
+    ${CONDAPATH}/conda-bld/${OUTPUT_ARCH}/*.tar.bz2 \
+    dummy@zigzah.com:~/${OUTPUT_CHANNEL}/${OUTPUT_ARCH}
+
+if [ $? == 0 ]; then
+	echo "Upload suceeded"
+else
+	echo "Upload failed"
+	exit 1
+fi
