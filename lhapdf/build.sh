39cf0be054f740e2c36b948fccb21d664a2139ed
Zahari <zaharid@gmail.com>
2018-07-20 15:46:38 +0100

    Call cython before make

--- lhapdf/build.sh
+++ lhapdf/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#This has to be here because autotools doesn't find the right cython
+(cd wrappers/python && cython --cplus lhapdf.pyx)
 ./configure --prefix=$PREFIX 
 make -j${CPU_COUNT}
 make install
 
 cd wrappers
-cython --cplus lhapdf.pyx
 make
 make install
