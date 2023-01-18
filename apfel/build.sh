a1266bcd9d1ea4ecb1412cddacf654891900f94a
Zahari Kassabov <zk261@cam.ac.uk>
2023-01-18 16:06:12 +0000

    Disable pywrap

--- apfel/build.sh
+++ apfel/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export FFLAGS=$(echo $FFLAGS | sed 's/-fopenmp/-fno-automatic/g')
-./configure --prefix=$PREFIX 
+./configure --prefix=$PREFIX  --disable-pywrap
 make -j${CPU_COUNT}
 make install
